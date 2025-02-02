6fdb59a0be3d59ebc983bd6089d454128c15c69d
bpowell461 <bpowell461@gmail.com>
2025-02-01 21:53:12 -0600

    Change hash again

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 5cc529634948ee3edc5951571fff56893a453ba1
+AESD_ASSIGNMENTS_VERSION = 3d20ec34837442a3c7698c59a934f4700a051ba0
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
